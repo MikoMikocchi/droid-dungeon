95b28e85706fc4de392a341880cf7cb0d2829058
Mikhail Matskevich <182345589+MikoMikocchi@users.noreply.github.com>
2026-01-27 16:00:10 +0300

    style(ui): reorder imports in HudRenderer

--- core/src/main/java/com/droiddungeon/ui/HudRenderer.java
+++ core/src/main/java/com/droiddungeon/ui/HudRenderer.java
@@ -1,8 +1,8 @@
 package com.droiddungeon.ui;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
