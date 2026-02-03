05895a2148c56f09bf1558131471a4ba4ad25530
Mikhail Matskevich <182345589+MikoMikocchi@users.noreply.github.com>
2026-02-03 12:01:48 +0300

    refactor(server): remove deprecated protocol mapper files
    Remove legacy ServerProtocolMapper and Protocol objects that have been superseded by the new codec architecture (com.droiddungeon.net.codec.ProtocolCodec and com.droiddungeon.net.dto)

--- server/src/main/scala/com/droiddungeon/server/ServerProtocolMapper.scala
+++
@@ -1,5 +0,0 @@
-package com.droiddungeon.server
-
-/** Legacy mapper; superseded by com.droiddungeon.net.codec.ProtocolCodec. */
-@deprecated("Use com.droiddungeon.net.codec.ProtocolCodec", "0.1.0")
-object ServerProtocolMapper
--- server/src/main/scala/com/droiddungeon/server/protocol.scala
+++
@@ -1,8 +0,0 @@
-package com.droiddungeon.server
-
-/** Legacy protocol definitions were moved to shared Java DTOs in core. */
-@deprecated(
-  "Use com.droiddungeon.net.dto and com.droiddungeon.net.codec",
-  "0.1.0"
-)
-object Protocol
